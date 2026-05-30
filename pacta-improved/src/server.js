a2f13ad6b59cd7a03aa75f067bcb088d76743483
Abdelghani Djedidi <codingenu@192.168.1.29>
2026-05-30 15:58:17 +0100

    fix errors10

--- pacta-improved/src/server.js
+++ pacta-improved/src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -21,7 +22,7 @@ const startServer = async () => {
   });
 };
 
-
+startServer()
 
 
 
